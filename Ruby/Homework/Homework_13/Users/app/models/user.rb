023fdd1551b91fc64b4ec7f25ce373a2d009ddc4
Stefan Mojsilovic <voodoomanic@gmail.com>
2015-06-14 16:46:19 +0200

    Ruby Homework_13

--- Ruby/Homework/Homework_13/Users/app/models/user.rb
+++ Ruby/Homework/Homework_13/Users/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true,
+                       length: { minimum: 3 }
+  validates :age, :numericality => { :greater_than_or_equal_to => 0 }
 end
--- Ruby/Homework/Homework_13/Users/config/routes.rb
+++ Ruby/Homework/Homework_13/Users/config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/show'
+  resources :users
 
-  get 'users/new'
-
-  get 'users/edit'
-
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/destroy'
+  root 'users#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
